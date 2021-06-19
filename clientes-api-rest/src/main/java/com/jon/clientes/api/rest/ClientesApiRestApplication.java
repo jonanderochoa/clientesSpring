5fefbeab5b28cbc6b05fee1e0935aa6accbeebaf
LAPTOP-CASA\jonan <jonanderochoa@gmail.com>
2021-06-19 23:59:07 +0200

    creado proyecto

---
+++ clientes-api-rest/src/main/java/com/jon/clientes/api/rest/ClientesApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.jon.clientes.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApiRestApplication.class, args);
+	}
+
+}
---
+++ clientes-api-rest/src/test/java/com/jon/clientes/api/rest/ClientesApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jon.clientes.api.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
