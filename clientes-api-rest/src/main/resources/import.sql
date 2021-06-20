c40aa8b00534e61d5fd9e22de0bce45369b3935b
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-06-20 23:40:51 +0200

    Datos de prueba
    Datos de prueba para la base de datos

---
+++ clientes-api-rest/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO `db_clientes`.`clientes` (`nombre`, `apellido`, `create_at`, `email`) VALUES ('HANNIBAL', 'SMITH', '2021-01-01', 'hannibalSmith@aTeam.com');
+INSERT INTO `db_clientes`.`clientes` (`nombre`, `apellido`, `create_at`, `email`) VALUES ('B.A.', 'BARACUS', '2021-02-02', 'baBaracus@aTeam.com');
+INSERT INTO `db_clientes`.`clientes` (`nombre`, `apellido`, `create_at`, `email`) VALUES ('TEMPELTON', 'PECK', '2021-03-03', 'tempeltonPeck@aTeam.com');
+INSERT INTO `db_clientes`.`clientes` (`nombre`, `apellido`, `create_at`, `email`) VALUES ('H.M.', 'MURDOCK', '2021-04-04', 'hmMurdock@aTeam.com');
+INSERT INTO `db_clientes`.`clientes` (`nombre`, `apellido`, `create_at`, `email`) VALUES ('RODERICK', 'DECKER', '2021-05-05', 'roderickDecker@aTeam.com');
+INSERT INTO `db_clientes`.`clientes` (`nombre`, `apellido`, `create_at`, `email`) VALUES ('FRANKIE', 'SANTANA', '2021-06-06', 'frankieSantana@aTeam.com');
