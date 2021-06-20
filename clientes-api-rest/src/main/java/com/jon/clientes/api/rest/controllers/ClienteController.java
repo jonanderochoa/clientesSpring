7a05e3860cc44eeaf3a98f83a7eb85ac13645dc2
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-06-20 23:40:21 +0200

    Controller de cliente
    Metodo findAll del controlador de clientes

---
+++ clientes-api-rest/src/main/java/com/jon/clientes/api/rest/controllers/ClienteController.java
@@ -0,0 +1,24 @@
+package com.jon.clientes.api.rest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jon.clientes.api.rest.models.entity.Cliente;
+import com.jon.clientes.api.rest.models.services.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping("/")
+	public List<Cliente> findAll() {
+		return clienteService.findAll();
+	}
+}
