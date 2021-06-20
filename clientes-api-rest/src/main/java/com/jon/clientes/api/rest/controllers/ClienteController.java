c45da0b240db3b944021a87b6103edfbebc178f6
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-06-20 23:51:00 +0200

    Añadir CORS
    Añadir CORS para poder conectar el backend de spring con el frontend de angular

--- clientes-api-rest/src/main/java/com/jon/clientes/api/rest/controllers/ClienteController.java
+++ clientes-api-rest/src/main/java/com/jon/clientes/api/rest/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.jon.clientes.api.rest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
 	
+	@CrossOrigin(origins = {"http://localhost:4200"})
 	@GetMapping("/")
 	public List<Cliente> findAll() {
 		return clienteService.findAll();
