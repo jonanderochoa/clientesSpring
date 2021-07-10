e1c7b2be0f3f007345044841bc1c19a65e1f639e
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-07-10 15:16:03 +0200

    Prepersist
    metodo del modelo que se ejecuta justo antes de persistirlo

--- clientes-api-rest/src/main/java/com/jon/clientes/api/rest/models/entity/Cliente.java
+++ clientes-api-rest/src/main/java/com/jon/clientes/api/rest/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -27,6 +28,11 @@ public class Cliente implements Serializable {
 	private String apellido;
 	
 	private String email;
+	
+	@PrePersist
+	private void prePersist() {
+		createAt = new Date();
+	}
 	
 	@Column(name="CREATE_AT")
 	@Temporal(TemporalType.DATE)
