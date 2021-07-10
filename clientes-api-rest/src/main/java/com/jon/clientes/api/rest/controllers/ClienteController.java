9753e6d243a056ae2c7eb9f7b43df43bb03b16ef
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-07-10 15:16:36 +0200

    Controller de cliente
    Metodos del controlador de cliente

--- clientes-api-rest/src/main/java/com/jon/clientes/api/rest/controllers/ClienteController.java
+++ clientes-api-rest/src/main/java/com/jon/clientes/api/rest/controllers/ClienteController.java
@@ -3,14 +3,21 @@ package com.jon.clientes.api.rest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jon.clientes.api.rest.models.entity.Cliente;
 import com.jon.clientes.api.rest.models.services.ClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
@@ -18,9 +25,24 @@ public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
 	
-	@CrossOrigin(origins = {"http://localhost:4200"})
 	@GetMapping("/")
 	public List<Cliente> findAll() {
 		return clienteService.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente findById(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente save(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 }
