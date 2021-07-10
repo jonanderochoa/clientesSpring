80c98457b0dda962bb410f04ae85edf8b87b59ad
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-07-10 16:03:00 +0200

    Update
    Metodo update del controller

--- clientes-api-rest/src/main/java/com/jon/clientes/api/rest/controllers/ClienteController.java
+++ clientes-api-rest/src/main/java/com/jon/clientes/api/rest/controllers/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,7 +42,18 @@ public class ClienteController {
 		return clienteService.save(cliente);
 	}
 	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		return clienteService.save(clienteActual);
+	}
+	
 	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		clienteService.delete(id);
 	}
